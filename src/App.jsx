cb7d610f707637ade8bb5bf23bacc480b30e0054
Tatyana Tupalo <tatyana080489t@gmail.com>
2021-07-03 08:41:10 +0300

    Добавила код

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Section from 'components/Section';
+import FeedbackOptions from 'components/FeedbackOptions';
+import Notification from 'components/Notification';
+import Statistics from 'components/Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = e => {
+    const name = e.target.name;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const result = good + neutral + bad;
+    return result;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const result = this.countTotalFeedback();
+    const { good } = this.state;
+    const percentage = (good * 100) / result;
+    return Math.round(percentage);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    const objKey = Object.keys(this.state);
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={objKey}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        {total === 0 ? (
+          <Notification message="No feedback given" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          </Section>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
